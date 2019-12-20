b0b168472e45ef2016e7eeb9c78148962428b846
yuryklim <yuryklim@gmail.com>
2019-12-20 17:26:55 +0200

    Passed username to our home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="https://instagram.fdnk1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fdnk1-1.fna.fbcdn.net&_nc_ohc=mRf5dxOKh6sAX8L9eJh&oh=e1b0405bebd800a7aed28a1080d48ab7&oe=5EAA2DD7" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
