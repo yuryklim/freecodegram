3c44a5e7f7327bfb0138a5bd9acfa67f7528b572
yuryklim <yuryklim@gmail.com>
2019-12-20 13:59:04 +0200

    Added main photo to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3">
+            <img src="https://instagram.fdnk1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fdnk1-1.fna.fbcdn.net&_nc_ohc=mRf5dxOKh6sAX8L9eJh&oh=e1b0405bebd800a7aed28a1080d48ab7&oe=5EAA2DD7" alt="">
+        </div>
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
