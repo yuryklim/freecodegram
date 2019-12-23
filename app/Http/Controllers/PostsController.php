5309c1676a87ab9c095f6a0bc9c5e5d708ab4e26
yuryklim <yuryklim@gmail.com>
2019-12-23 17:02:15 +0200

    Made images as links

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -36,4 +36,9 @@ class PostsController extends Controller
 
         return redirect('/profile/' . auth()->user()->id);
     }
+
+    public function show(\App\Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
 }
