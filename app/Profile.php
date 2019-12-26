fb80077c9e772768b3bc20133507f6756fe49870
yuryklim <yuryklim@gmail.com>
2019-12-26 12:36:23 +0200

    Added follow button works

--- app/Profile.php
+++ app/Profile.php
@@ -13,6 +13,12 @@ class Profile extends Model
         $imagePath = ($this->image) ?  $this->image : '/profile/завантаження.png';
         return '/storage/' . $imagePath;
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
