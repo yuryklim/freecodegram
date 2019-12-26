8ee5d783e1f4540bafd491e39a41a1ea6308cbd6
yuryklim <yuryklim@gmail.com>
2019-12-26 13:50:03 +0200

    Added pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,5 +24,10 @@
             </div>
         </div>
     @endforeach
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
+        </div>
 </div>
 @endsection
