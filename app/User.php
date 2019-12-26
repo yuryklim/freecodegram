46299c998238bdfd8729116275b4f9e0c358c06b
yuryklim <yuryklim@gmail.com>
2019-12-26 15:31:58 +0200

    Added possibility to send welcome mail

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -40,10 +42,13 @@ class User extends Authenticatable
     protected static function boot()
     {
         parent::boot();
+
         static::created(function($user){
             $user->profile()->create([
                 'title' => $user->username,
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
     }
 
